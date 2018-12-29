c79d21165f0038f8aae4345eba2d059004763791
Piotr Ulanicki <piotr.ulanicki4@gmail.com>
2018-12-29 21:06:53 +0100

    added Details/Delete action/view

--- MusicList/MusicList/Controllers/MusicsController.cs
+++ MusicList/MusicList/Controllers/MusicsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MusicList.Models;
 
 namespace MusicList.Controllers
@@ -21,7 +22,7 @@ namespace MusicList.Controllers
             return View(_db.Musics.ToList());
         }
 
-        //Get : Music/Create
+        //Get : Musics/Create
         public IActionResult Create()
         {
             return View();
@@ -39,6 +40,82 @@ namespace MusicList.Controllers
             }
             return View(music);
         }
+
+        //Details : Musics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var music = await _db.Musics.SingleOrDefaultAsync(m => m.Id == id);
+            if(music == null)
+            {
+                return NotFound();
+            }
+            return View(music);
+        }
+
+        //Edit : Musics/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var music = await _db.Musics.SingleOrDefaultAsync(m => m.Id == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+            return View(music);
+        }
+
+        //POST : Musics/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Music music)
+        {
+            if(id != music.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Update(music);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(music);
+        }
+
+        //Delete : Musics/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var music = await _db.Musics.SingleOrDefaultAsync(m => m.Id == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+            return View(music);
+        }
+
+        //Post : Musics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveMusic(int id)
+        {
+            var music = await _db.Musics.SingleOrDefaultAsync(m => m.Id == id);
+            _db.Musics.Remove(music);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
 
         protected override void Dispose(bool disposing)
         {
